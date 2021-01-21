70582ecfa83890b29321bb13af21750447da6f5d
satriopurbo <puka@DESKTOP-72GHHEB.localdomain>
2021-01-21 14:11:35 +0700

    emit Temporary Screening

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const app = express()
 const morgan = require('morgan')
 const cors =require('cors')
 const routing = require('./routing/index')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+
+io.on('connection',(socket)=>{
+  console.log('user connected');
+
+  socket.on('disconnect',function(){
+    console.log('user DC')
+  })
+})
 
 
 app.use(morgan('dev'))
@@ -13,6 +24,13 @@ app.use(express.json())
 app.use('/', routing)
 
 const port = 3005
+http.listen(port, () => {
+  console.log(`socket telah tersambung pada port : ${port}`)
+});
+
  app.listen(port, () => {
    console.log(`telah tersambung pada port : ${port}`)
- })
+ });
+
+ 
+ module.exports=io
