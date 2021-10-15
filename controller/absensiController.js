04d397a5ab3c9be5a39af58476fa6b2c3cccd73f
satriopurbo <satrio_leonheart@yahoo.com>
2021-10-15 15:14:58 +0700

    qwe

--- controller/absensiController.js
+++ controller/absensiController.js
@@ -86,7 +86,7 @@ class Controller{
 
     static async absensiByKaryawanId(req,res){
         const{karyawanId,bulan,tahun}=req.params
-        let data = await sq.query(`select k.nama ,a."tanggalAbsen" ,a.absen from karyawans k join absensis a ON k.id = a."karyawanId" where k.id= ${karyawanId} and  EXTRACT(MONTH FROM a."tanggalAbsen"+ interval '7 hour') = ${bulan} and EXTRACT(YEAR FROM a."tanggalAbsen" + interval '7 hour') =${tahun} group by k.id,a."tanggalAbsen",a.absen order by "tanggalAbsen" `)
+        let data = await sq.query(`select k.nama ,a."tanggalAbsen" ,a.absen from karyawans k join absensis a ON k.id = a."karyawanId" where k.id= ${karyawanId} and  EXTRACT(MONTH FROM a."tanggalAbsen"+ interval '7 hour') = ${bulan} and EXTRACT(YEAR FROM a."tanggalAbsen" + interval '7 hour') =${tahun} group by k.id,a.id order by "tanggalAbsen" `)
         res.json({data:data[0]})
     }
 
